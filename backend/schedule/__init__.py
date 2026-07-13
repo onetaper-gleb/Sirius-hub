099e976c44d6826f667249bce339c60fdf8c38e9
alenkablok <284977376+alenkablok@users.noreply.github.com>
2026-07-13 10:47:00 +0000

    style: auto-format code

--- backend/schedule/__init__.py
+++ backend/schedule/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-from .group_routes import router as group_router
 from .classrooms_routes import router as classrooms_router
+from .group_routes import router as group_router
 
 router = APIRouter()
 
