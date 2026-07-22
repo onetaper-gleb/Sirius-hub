635af13a1637c472eb0b9083f4ab3e7b42c41e79
alenkablok <284977376+alenkablok@users.noreply.github.com>
2026-07-22 07:49:19 +0000

    style: auto-format code

--- backend/forum/topic_routes.py
+++ backend/forum/topic_routes.py
@@ -1,9 +1,9 @@
 from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy import delete
 
 from auth.auth_routes import get_current_user, require_council_role
 from database.database import get_db
@@ -19,6 +19,7 @@ topic_router = APIRouter(
 )
 
 logger = set_logger()
+
 
 async def _get_db_user(db: AsyncSession, uid: str) -> User | None:
     result = await db.execute(select(User).where(User.id == uid))
@@ -42,13 +43,15 @@ async def get_comments(
     db: AsyncSession = Depends(get_db),
 ):
 
-    logger.info(f'User requested comments for topic {topic_id}')
+    logger.info(f"User requested comments for topic {topic_id}")
 
     topic = await _get_db_topic(db, topic_id)
-    
+
     if topic is None:
 
-        logger.warning(f'User tried to get comments from a non-existent topic {topic_id}')
+        logger.warning(
+            f"User tried to get comments from a non-existent topic {topic_id}"
+        )
         raise HTTPException(status_code=404, detail="Topic does not exist")
 
     comments_schemas = await db.execute(
@@ -85,7 +88,7 @@ async def get_comments(
             }
         )
 
-    logger.info(f'User successfully received comments for a topic {topic_id}')
+    logger.info(f"User successfully received comments for a topic {topic_id}")
     return comments_schemas
 
 
@@ -95,19 +98,19 @@ async def create_comment(
     user: dict = Depends(get_current_user),
     db: AsyncSession = Depends(get_db),
 ):
-    logger.info(f'User is trying to create a comment in topic {request.topic_id}')
+    logger.info(f"User is trying to create a comment in topic {request.topic_id}")
 
     content = request.content.strip()
 
     if not 1 < len(content) < 200:
 
-        logger.warning(f'User tried to create an invalid comment')
+        logger.warning(f"User tried to create an invalid comment")
         raise HTTPException(status_code=400, detail="Comment content is invalid")
 
     topic = await _get_db_topic(db, request.topic_id)
     if topic is None:
 
-        logger.warning(f'User tried to create a comment for a non-existent topic')
+        logger.warning(f"User tried to create a comment for a non-existent topic")
         raise HTTPException(status_code=404, detail="Topic does not exist")
 
     reply_to_author = None
@@ -116,12 +119,12 @@ async def create_comment(
 
         if parent_comment is None:
 
-            logger.warning(f'User tried to reply to a non-existent comment')
+            logger.warning(f"User tried to reply to a non-existent comment")
             raise HTTPException(status_code=404, detail="Parent comment not found")
 
         if parent_comment.topic_id != request.topic_id:
 
-            logger.warning(f'User tried to reply to a comment from another topic')
+            logger.warning(f"User tried to reply to a comment from another topic")
             raise HTTPException(
                 status_code=400, detail="Can not reply to a comment from another topic"
             )
@@ -144,7 +147,9 @@ async def create_comment(
 
     author = await _get_db_user(db, new_comment.user_id)
 
-    logger.info(f'User successfully created a comment {new_comment.id} in topic {request.topic_id}')
+    logger.info(
+        f"User successfully created a comment {new_comment.id} in topic {request.topic_id}"
+    )
 
     return {
         "content": new_comment.content,
@@ -162,26 +167,23 @@ async def delete_comment(
     db: AsyncSession = Depends(get_db),
 ):
     id_user = user.get("uid")
-    logger.info(f'User is trying to delete a comment {comment_id}')
+    logger.info(f"User is trying to delete a comment {comment_id}")
 
     comment = await _get_db_comment(db, comment_id)
 
     if comment is None:
 
-        logger.warning(f'User is trying to delete a non-existent comment')
+        logger.warning(f"User is trying to delete a non-existent comment")
         raise HTTPException(status_code=404, detail="Comment not found")
-    
+
     if comment.user_id != id_user:
         await require_council_role(db=db, user=user)
 
-    await db.execute(
-        delete(Comments)
-        .where(Comments.parent_comment_id == comment_id)
-   )
+    await db.execute(delete(Comments).where(Comments.parent_comment_id == comment_id))
 
     await db.delete(comment)
     await db.commit()
 
-    logger.info(f'User successfully deleted a comment {comment_id} and its replies')
+    logger.info(f"User successfully deleted a comment {comment_id} and its replies")
 
     return {"message": "Comment deleted successfully"}
